d3b2f551ac141fe2327bb196db39af8df037e013
Rafael Duarte Gervásio da Silva <rafael.dgs.1993@gmail.com>
2013-10-19 12:26:12 -0300

    Apagando arquivos PHP da pasta View (estão todos na pasta PHP)

--- Sistema/Views/ImagemView.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("..\Controllers\ImagemController.php");
-header("Content-type:image/*");
-$controller = new ImagemController();
-
-$lista = $controller->readImagem($_GET);
-
-echo $imagem = $lista[0]->imagem;
-
